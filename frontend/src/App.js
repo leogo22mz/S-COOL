93f66e54c77e3d04268f7d77f523457a12ddfb92
leogo22mz <leogo2206@gmail.com>
2024-03-05 11:07:56 +0000

    My very first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          MY VERY FIRST CHANGE AND GIT PUSH
         </p>
         <a
           className="App-link"
