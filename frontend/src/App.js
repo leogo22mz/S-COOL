e56330ea9b8be13626673384e5169e78bb787c2b
leogo22mz <leogo2206@gmail.com>
2024-03-05 11:41:59 +0000

    Implemented the first page: Landing Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage/LandingPage.js';
+import { useEffect, useState } from 'react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<LandingPage />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+
+
+function LandingPage() {
+
+
+    return (
+
+        <div>
+            <h1>LANDING PAGE</h1>
+            <p>TEST FOR LANDING PAGE</p>
+        </div>
+    );
+}
+
+export default LandingPage;
