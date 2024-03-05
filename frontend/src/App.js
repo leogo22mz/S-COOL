ef35362de40d56420907ce2fb808cbd4daa812ee
leogo22mz <leogo2206@gmail.com>
2024-03-05 11:09:44 +0000

    My second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          MY VERY FIRST CHANGE AND GIT PUSH
-        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
