4e6a2ad7ff8853b9e8e2916cd5410918a333ab8f
leogo22mz <leogo2206@gmail.com>
2024-03-08 09:04:03 +0000

    Implemented ContactUs page design

--- frontend/src/pages/ContactUsPage/ContactUsPage.js
+++ frontend/src/pages/ContactUsPage/ContactUsPage.js
@@ -1,14 +1,57 @@
+import React from 'react';
+import { Form, Input, Button } from 'antd';
 import './ContactUsPage.css';
 import TopBarDropdown from '../../components/TopBarDropdown';
+import logo from '../../assets/img/logo.svg';
 
 function ContactUsPage() {
+    // Function to handle form submission
+    const onFinish = (values) => {
+        console.log('Received values of form:', values);
+    };
+
+    // Function to handle form submission failure
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
 
     return (
-
-        <div>
+        <div className="contact-page">
             <TopBarDropdown />
-            <h1>ContactUsPage</h1>
-            <p>TEST FOR ContactUsPage</p>
+            <div className="logo-section">
+                <img src={logo} alt="Logo" />
+            </div>
+            <br/>
+            <div className="contact-form-container">
+            <h1 className="contact-title">Contact Us</h1>
+
+                <Form
+                    name="contact_form"
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    layout="vertical"
+                    className="contact-form"
+                >
+                    <Form.Item
+                        name="message"
+                        label="Leave us a message:"
+                        className="form-item-message"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your message!',
+                            },
+                        ]}
+                    >
+                        <Input.TextArea className="message-textarea" />
+                    </Form.Item>
+                    <Form.Item className="form-item-submit">
+                        <Button type="primary" htmlType="submit" className="submit-button">
+                            SEND
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
         </div>
     );
 }
