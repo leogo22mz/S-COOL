450a2dff79aa299081c72ad3795a5e76140eb4ed
leogo22mz <leogo2206@gmail.com>
2024-03-12 09:46:07 +0000

    Development

--- frontend/src/pages/CourseContent/CourseContent.js
+++ frontend/src/pages/CourseContent/CourseContent.js
@@ -12,17 +12,12 @@ function CourseContent() {
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis erat ante, non eleifend diam auctor in.
                     Pellentesque tortor odio, posuere quis sem vitae, condimentum aliquet metus.
                 </p>
-                <div className="course-content-actions">
-                    <div className="course-content-buttons-top">
-                        <button className="course-content-button">read more</button>
-                        <button className="course-content-button">ask a question</button>
-                    </div>
-                    <div className="course-content-buttons-bottom">
-                        <button className="course-content-button">third action</button>
-                    </div>
-                </div>
             </div>
-            <a href="#" className="course-content-back">← back</a>
+            <div className="course-content-buttons-triangle">
+                <button className="course-content-button button-left">read more</button>
+                <button className="course-content-button button-center">ask a question</button>
+                <button className="course-content-button button-right">third action</button>
+            </div>
         </div>
     );
 }
